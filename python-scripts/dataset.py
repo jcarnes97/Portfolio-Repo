2778a68436c08fbdcfc2dbbed84e9caa264ead33
Jordan Carnes <64877522+jcarnes97@users.noreply.github.com>
2026-04-10 03:02:29 -0500

    Delete python-scripts/dataset.py

--- python-scripts/dataset.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(r'/home/jcarnes/Documents/Conditions_Contributing_to_COVID-19_Deaths__by_State_and_Age__Provisional_2020-2023 (copy).csv')
-
-mean1 = df['Age Group'].mean
-mean2 = df['COVID-19 Deaths'].mean
-print("Mean Age Group", mean1)
-print("Mean COVID Deaths", mean2)
